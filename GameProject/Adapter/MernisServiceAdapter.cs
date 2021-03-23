592841cdf41a685ae4a5e290c9ee5633608ef129
Musa Erkek <musaerkek1@outlook.com>
2021-03-23 16:12:55 +0300

    Add project files.

---
+++ GameProject/Adapter/MernisServiceAdapter.cs
@@ -0,0 +1,19 @@
+﻿using GameProject.Abstract;
+using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MernisServiceReference;
+
+namespace GameProject.Adapter
+{
+    class MernisServiceAdapter : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            var result= client.TCKimlikNoDogrulaAsync(Convert.ToInt64(gamer.IdentityNumber),gamer.FirstName.ToUpper(), gamer.LastName.ToUpper(),gamer.BirthYear.Year).Result;
+            return result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using GameProject.Adapter;
+using GameProject.Concrete;
+using GameProject.Entities;
+using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gamer gamer1 = new Gamer()
+            {
+                Id = 1,
+                FirstName = "Musa",
+                LastName = "Erkek",
+                BirthYear = new System.DateTime(1998,1,1),
+                IdentityNumber = "12345678910"
+            };
+
+            GamerManager gamerManager = new GamerManager(new MernisServiceAdapter());
+            gamerManager.Add(gamer1);
+
+            Game game1 = new Game() { Id = 1, Name = "Witcher3", Price = 50 };
+
+            GameManager gameManager = new GameManager();
+            gameManager.Add(game1);
+          
+            Campaign campaign1 = new Campaign() { Id = 1, Name ="Black Friday", Discount = 30 };
+
+            CampaignManager campaignManager = new CampaignManager();
+            campaignManager.Add(campaign1);
+        }
+    }
+}
